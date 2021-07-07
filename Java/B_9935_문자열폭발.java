663f50acde4211b5b956c4469908f68232fb45fa
qjatn1009 <a43727702@gmail.com>
2021-07-07 21:50:49 +0900

    2021-07-07 백준 9935 문자열 폭발

---
+++ Java/B_9935_문자열폭발.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B_9935_¹®ÀÚ¿­Æø¹ß {
+
+	static String text, pattern;
+	static Stack<Character> stack = new Stack<>();
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		text = br.readLine();
+		pattern = br.readLine();
+
+		for (int i = 0; i < text.length(); i++) {
+			stack.add(text.charAt(i));
+			while (stack.size() >= pattern.length() && check()) {
+				for (int j = 0; j < pattern.length(); j++) {
+					stack.pop();
+				}
+			}
+		}
+		if (stack.size() == 0) {
+			sb.append("FRULA");
+		} else {
+			for (Character c : stack)
+				sb.append(c);
+		}
+		System.out.println(sb);
+	}
+
+	static boolean check() {
+		int len = stack.size();
+		int plen = pattern.length();
+		for (int i = 0; i < plen; i++) {
+			if (stack.get(len - plen + i) != pattern.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
